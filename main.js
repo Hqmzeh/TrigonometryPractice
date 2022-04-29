5c6fa13db053d2baaf713dda35d8c22586fa23cf
Hqmzeh <hamzehhazeem8@gmail.com>
2022-04-29 20:00:48 +0300

    made the buttons togglable, added the calculator function to get the answer, just need to add question randomizer and check it with user's input

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-function disableButton(ID) {
-    // Select the element with id "theButton" and disable it
-    document.getElementById(ID).disabled = true;
-  }
+var variables = [1, 1, 1, 1, 1, 1]
+
+function disableButton(ID, arrayToBeReplaced, value) {
+    if (document.getElementById(ID).style.backgroundColor == "transparent") {
+        document.getElementById(ID).style.backgroundColor = "lightgreen"
+        variables.splice(arrayToBeReplaced, 1, value)
+    } else {
+        document.getElementById(ID).style.backgroundColor = "transparent"
+        variables.splice(arrayToBeReplaced, 1, 1)
+    }
+}
+
+var answer = 1
+
+function submitAnswer() {
+    var multiplicies = [variables[0], 1, 1]
+    if (variables[1] == "√") {
+        var t = Math.sqrt(variables[2])
+        multiplicies.splice(1, 1, t)
+    } else {
+        multiplicies.splice(1, 1, variables[2])
+    }
+
+    if (variables[4] == "√") {
+        var y = Math.sqrt(variables[5])
+        multiplicies.splice(2, 1, y)
+    } else {
+        multiplicies.splice(2, 1, variables[5])
+    }
+    answer *= multiplicies.reduce((a, b) => a * b, 1)
+    alert(answer)
+}
