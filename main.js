5cfbbd6841aec3d0b11056f8d6f1adc6f5f0e061
Hqmzeh <hamzehhazeem8@gmail.com>
2022-04-30 04:08:58 +0300

    'Undefined' now works as intended, and the timer now stops when getting wrong or right answer. Other small changes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var randomTrigonometricFunction = trigonometricFunction[Math.floor(Math.random()
 var randomCoterminal = 360 * coterminal[Math.floor(Math.random() * coterminal.length)] //randomize stuff from the arrays at top to make a unique value each refresh 
 var randomTheta = theta[Math.floor(Math.random() * theta.length)] + randomCoterminal
 var radianTheta = randomTheta * Math.PI / 180 //converting from degree to radians because Math.sin requires radians
-
+var tanExceptions = [90, 270, 450, 630]
 //here is some stuff that is standard nowadays so I copied it because I have no idea how this work. it add sleep function
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
@@ -17,14 +17,25 @@ function sleep(ms) {
 async function getQuestion() {
     document.getElementById("theQuestion").innerText = randomTrigonometricFunction + "(" + randomTheta + ")" //add the question to the screen
 
-    for (i = 15; i > -1; --i) { //a countdown that uses sleep the "async" at the top make this work remove it and it wont
+    timer: for (i = 15; i > -1; --i) { //a countdown that uses sleep the "async" at the top make this work remove it and it wont. used a label for this function
         document.getElementById("theTimer").innerText = "00:" + i
+
+        if (document.getElementById("result").innerHTML == "Correct") { //breaking timer if we got the answer right or wrong
+            document.getElementById("theTimer").style.color = "green"
+            break timer;
+        } else if (document.getElementById("result").innerHTML == "Incorrect") {
+            document.getElementById("theTimer").style.color = "red"
+            break timer;
+        }
+
         await sleep(1000);
 
+
         if (i == 0) {
+
             document.getElementById("result").style.color = "red"
             document.getElementById("result").innerHTML = "Timed Out"
-            document.getElementById("Submit").value = "Reset"
+            document.getElementById("Submit").value = "New Question"
         }
 
     }
@@ -43,7 +54,7 @@ function disableButton(ID, arrayToBeReplaced, value) {
 //some annoying if statments that checks for √ and root the number after it
 //and some more that does the same but with sin. I used parseFloat and .toFixed to make the answer as 0.0 to help with correcting the questions
 function submitAnswer() {
-    if (document.getElementById("Submit").value == "Reset") {
+    if (document.getElementById("Submit").value == "New Question") {
         window.location.reload("Refresh")
     } else {
 
@@ -61,7 +72,7 @@ function submitAnswer() {
         } else {
             multiplicies.splice(2, 1, variables[5])
         }
-        userAnswer *= multiplicies.reduce((a, b) => a * b, 1)
+
         if (randomTrigonometricFunction == "sin") {
             answer = parseFloat(Math.sin(radianTheta).toFixed(1))
             //alert("sin for " + randomTheta +" is " + answer)
@@ -72,16 +83,22 @@ function submitAnswer() {
             answer = parseFloat(Math.tan(radianTheta).toFixed(1))
             //alert("tan for " + randomTheta +" is "+ answer)
         }
+        //randomTheta == 90 || 270 || 450 || 630
+        if (variables[0] == 2 && randomTrigonometricFunction == "tan" && tanExceptions.indexOf(randomTheta) !== -1) {
+            userAnswer = answer
+        } else {
+            userAnswer *= multiplicies.reduce((a, b) => a * b, 1)
+        }
 
         //finally we just check the answer with yet another if statment...
         if (answer == parseFloat(userAnswer.toFixed(1))) {
             document.getElementById("result").style.color = "green"
             document.getElementById("result").innerHTML = "Correct"
-            document.getElementById("Submit").value = "Reset"
+            document.getElementById("Submit").value = "New Question"
         } else {
             document.getElementById("result").style.color = "red"
             document.getElementById("result").innerHTML = "Incorrect"
-            document.getElementById("Submit").value = "Reset"
+            document.getElementById("Submit").value = "New Question"
         }
     }
 }
